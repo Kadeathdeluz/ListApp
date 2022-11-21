fc07d5b9644c6a61d8ed8a61467c3433321377f6
Kadeathdeluz <roldansanchismartinez@gmail.com>
2022-11-21 23:06:17 +0100

    Initial commit

---
+++ app/src/main/java/proyectos/kade/listapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package proyectos.kade.listapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
